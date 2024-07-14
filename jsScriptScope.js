8f0f9043d5cf5816828309dfb5675c3cf5311cb9
Harish Kumar S <150892097+harishkumar7558@users.noreply.github.com>
2024-07-14 02:16:42 -0700

    Add files via upload

---
+++ jsScriptScope.js
@@ -0,0 +1,10 @@
+
+handlefunction();
+
+function handlefunction() {
+    let cars="volvo";
+
+    document.getElementById("demo1").innerHTML= cars;
+}
+
+document.getElementById("demo2").innerHTML=typeof cars;
